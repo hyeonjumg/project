47c6b12f9b287d35e209653e8bdf35c0ada3c5cb
hyeonjumg <45448065+hyeonjumg@users.noreply.github.com>
2018-12-18 16:35:28 +0900

    Create Custom.java

---
+++ classes/custom/Custom.java
@@ -0,0 +1,52 @@
+package custom;
+
+public class Custom {
+	
+	private int customID;
+	private String customTitle;
+	private String userID;
+	private String customDate;
+	private String customContent;
+	private int customAvailable;
+	
+	
+	public int getCustomID() {
+		return customID;
+	}
+	public void setCustomID(int customID) {
+		this.customID = customID;
+	}
+	public String getCustomTitle() {
+		return customTitle;
+	}
+	public void setCustomTitle(String customTitle) {
+		this.customTitle = customTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getCustomDate() {
+		return customDate;
+	}
+	public void setCustomDate(String customDate) {
+		this.customDate = customDate;
+	}
+	public String getCustomContent() {
+		return customContent;
+	}
+	public void setCustomContent(String customContent) {
+		this.customContent = customContent;
+	}
+	public int getCustomAvailable() {
+		return customAvailable;
+	}
+	public void setCustomAvailable(int customAvailable) {
+		this.customAvailable = customAvailable;
+	}
+	
+	
+
+}
